3ff34c91788e23408a20677a5b62038af3fdcf86
Emily Morgado <emilyannemorgado@gmail.com>
2020-05-26 22:21:38 -0700

    Iterating over data to display cards. Display navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+
+import Navigation from './components/navigation'
+import Card from './components/card'
+import data from './fakeData'
+import './App.css'
+
+const recipes = data.recipes.map(recipe =>
+  <li>
+    <Card
+      title={recipe.title}
+    />
+  </li>
+)
 
 function App() {
   return (
     <div>
+       <h1>PROJECTS</h1>
+       <Navigation />
+       <ul className='container'>{recipes}</ul>
     </div>
   );
 }

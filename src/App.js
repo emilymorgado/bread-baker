1a8d071729172a21e5588cb28bc0c1505d2b1b9a
Emily Morgado <emilyannemorgado@gmail.com>
2020-06-01 18:24:12 -0700

    Move menu related state, jsx, functions out of App component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,24 @@
 import React, {Component} from 'react'
 
 import Navigation from './components/navigation'
-import Card from './components/card'
+import Menu from './components/menu'
 import data from './fakeData'
 import './App.css'
 
 class App extends Component {
   state = {
     userUnlocked: new Set(data.user.unlocked),
-    showAll: false
+    showAll: false,
+    showMenu: true,
   }
 
   toggleDisplay = () => {
     this.setState({showAll: !this.state.showAll})
   }
 
-  allRecipes = data.recipes.map(recipe => {
-    let unlocked = this.state.userUnlocked.has(recipe.id)
-    if (unlocked) {
-      unlocked = data.user.projectStatuses[recipe.id]
-    }
-    return (
-      <li key={recipe.id}>
-        <Card
-          title={recipe.title}
-          unlocked={unlocked}
-        />
-      </li>
-    )
-  })
-
-  activeRecipes = data.recipes.map(recipe => {
-    let unlocked = this.state.userUnlocked.has(recipe.id)
-    if (unlocked) {
-      unlocked = data.user.projectStatuses[recipe.id]
-      return (
-        <li key={recipe.id}>
-          <Card
-            title={recipe.title}
-            unlocked={unlocked}
-          />
-        </li>
-      )
-    }
-    return false
-  })
-
+  displayNextStep = () => {
+    this.setState({showMenu: !this.state.showMenu})
+  }
 
   render() {
     return (
@@ -55,9 +28,10 @@ class App extends Component {
           showAll={this.state.showAll}
           toggleDisplay={this.toggleDisplay}
          />
-         <ul className='container'>
-         {this.state.showAll ? this.allRecipes : this.activeRecipes}
-         </ul>
+         {this.state.showMenu ?
+           <Menu showAll={this.state.showAll} /> :
+           <Menu showAll={this.state.showAll} />
+         }
       </div>
     )
   }

85b4d473d4c7aae2ef969bf1824fae0f849ce677
Emily Morgado <emilyannemorgado@gmail.com>
2020-06-11 11:33:27 -0700

    Refactor out description field and make it direction 0. Update UI to reflect this.

--- src/components/directions.js
+++ src/components/directions.js
@@ -3,13 +3,11 @@ import React from 'react';
 const Directions = props => {
   return (
     <div className='directions'>
-      <h2>{props.step === 0 ? `Make sure you have:`
-        : props.step ? `Next Step:`
-        : `In this recipe:`}
+      <h2>{props.step === 0 ? `In this recipe:`
+        : props.step === 1 ? `Make sure you have:`
+        : `Next Step:`}
       </h2>
-      <p>{props.step ?
-        props.recipe.directions[props.step].direction :
-        props.recipe.description}
+      <p>{props.step === 1 ? "STEP ONE" : props.recipe.directions[props.step].direction}
       </p>
       <button>Get Started</button>
       <button onClick={()=>{props.displayDirections()}}>

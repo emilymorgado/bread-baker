c686f8697ad410bcb2e0f2af1a2312041306c766
Emily Morgado <emilyannemorgado@gmail.com>
2020-06-19 09:11:50 -0700

    Rework menu component to access and render firebase data

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,55 +1,70 @@
 import React, {Component} from 'react'
 
+import firebase from '../firestore'
 import Card from './card'
 import data from '../fakeData'
 
 class Menu extends Component {
   state = {
     userUnlocked: new Set(Object.keys(data.user.unlocked)),
+    // TODO: store in future
+    // activeRecipes: [],
+    allRecipes: [],
   }
 
-  allRecipes = data.recipes.map(recipe => {
-    let unlocked = this.state.userUnlocked.has(recipe.id)  //bool
-    if (unlocked) {
-      unlocked = data.user.unlocked[recipe.id] //Number for status and step
-    }
-    return (
-      <li key={recipe.id}>
-        <Card
-          title={recipe.title}
-          unlocked={unlocked}
-          displayDirections={this.props.displayDirections}
-          recipe={recipe}
-        />
-      </li>
-    )
-  })
+  componentDidMount() {
+    // TODO: default view is active, not all
+    firebase.collection('recipes').get().then(recipes => {
+      const allRecipes = []
+      recipes.forEach(recipe => {
+        allRecipes.push({'id': recipe.id, 'recipe': recipe.data()})
+      })
+      this.setState({ allRecipes: allRecipes})
+    })
+  }
 
-  // TODO: query/fetch just the unlocked recipes and iterate over that
-  activeRecipes = data.recipes.map(recipe => {
-    let unlocked = this.state.userUnlocked.has(recipe.id)  //bool
-    if (unlocked) {
-      unlocked = data.user.unlocked[recipe.id] //Number for status and step
+  render() {
+    const displayAllRecipes = (this.state.allRecipes).map(recipe => {
+      let unlocked = this.state.userUnlocked.has(recipe.id)
+      if (unlocked) {
+        unlocked = data.user.unlocked[recipe.id] //Number for status and step
+      }
       return (
         <li key={recipe.id}>
           <Card
             title={recipe.title}
-            recipe={recipe}
             unlocked={unlocked}
             displayDirections={this.props.displayDirections}
+            recipe={recipe}
           />
         </li>
       )
-    }
-    return false
-  })
+    })
 
-  render() {
+    // TODO: query/fetch just the unlocked recipes and iterate over that
+    const displayActiveRecipes = this.state.allRecipes.map(recipe => {
+      let unlocked = this.state.userUnlocked.has(recipe.id)
+      if (unlocked) {
+        unlocked = data.user.unlocked[recipe.id] //Number for status and step
+        return (
+          <li key={recipe.id}>
+            <Card
+              title={recipe.title}
+              recipe={recipe}
+              unlocked={unlocked}
+              displayDirections={this.props.displayDirections}
+            />
+          </li>
+        )
+      }
+      return false
+    })
+
     return (
       <ul className='container-cards'>
-      {this.props.showAll ?
-        this.allRecipes :
-        this.activeRecipes}
+        {this.props.showAll ?
+          displayAllRecipes :
+          displayActiveRecipes}
       </ul>
     )
   }

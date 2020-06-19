e5c82297e11bc269380e706d675a7c72eddf31cc
Emily Morgado <emilyannemorgado@gmail.com>
2020-06-19 13:59:54 -0700

    Replace fake data in menu component with firebase data. Clean up comments. Restructure state.

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,24 +2,37 @@ import React, {Component} from 'react'
 
 import firebase from '../firestore'
 import Card from './card'
-import data from '../fakeData'
 
 class Menu extends Component {
   state = {
-    userUnlocked: new Set(Object.keys(data.user.unlocked)),
-    // TODO: store in future
-    // activeRecipes: [],
+    userUnlocked: new Set(),
+    // TODO: store in future - activeRecipes: [],
     allRecipes: [],
+    user: null,
   }
 
   componentDidMount() {
-    // TODO: default view is active, not all
-    firebase.collection('recipes').get().then(recipes => {
+    let recipesRef = firebase.collection('recipes')
+    recipesRef.get().then(recipes => {
       const allRecipes = []
+
       recipes.forEach(recipe => {
         allRecipes.push({'id': recipe.id, 'recipe': recipe.data()})
       })
-      this.setState({ allRecipes: allRecipes})
+      this.setState({allRecipes: allRecipes})
+    })
+
+    let userRef = firebase.collection('users').doc('rCHHc9Pi7u6OYMaEF9ez')
+    userRef.get().then(doc => {
+      if (!doc.exists) {
+        console.warn('No such document!');
+      } else {
+        this.setState({user: doc.data()})
+        this.setState({userUnlocked: new Set(Object.keys(doc.data().unlocked))})
+      }
+    })
+    .catch(err => {
+      console.warn('Error getting document', err);
     })
   }
 
@@ -27,30 +40,29 @@ class Menu extends Component {
     const displayAllRecipes = (this.state.allRecipes).map(recipe => {
       let unlocked = this.state.userUnlocked.has(recipe.id)
       if (unlocked) {
-        unlocked = data.user.unlocked[recipe.id] //Number for status and step
+        unlocked = this.state.user.unlocked[recipe.id] //Number for status and step
       }
       return (
         <li key={recipe.id}>
           <Card
-            title={recipe.title}
+            title={recipe.recipe.title}
             unlocked={unlocked}
             displayDirections={this.props.displayDirections}
-            recipe={recipe}
+            recipe={recipe.recipe}
           />
         </li>
       )
     })
 
-    // TODO: query/fetch just the unlocked recipes and iterate over that
     const displayActiveRecipes = this.state.allRecipes.map(recipe => {
       let unlocked = this.state.userUnlocked.has(recipe.id)
       if (unlocked) {
-        unlocked = data.user.unlocked[recipe.id] //Number for status and step
+        unlocked = this.state.user.unlocked[recipe.id]
         return (
           <li key={recipe.id}>
             <Card
-              title={recipe.title}
-              recipe={recipe}
+              title={recipe.recipe.title}
+              recipe={recipe.recipe}
               unlocked={unlocked}
               displayDirections={this.props.displayDirections}
             />

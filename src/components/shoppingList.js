7738732e5e5e80c85e7907498feb753d44f3c7a7
Emily Morgado <emilyannemorgado@gmail.com>
2020-06-30 13:58:30 -0700

    Add button when all shopping items have been obtained

--- src/components/shoppingList.js
+++ src/components/shoppingList.js
@@ -1,19 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { PropTypes } from 'prop-types'
 
 class ShoppingList extends Component {
   state = {
     checked: new Array(this.props.items.direction.length).fill(false),
+    numberChecked: 0, // will be a number based on data in firebase
+    obtainedAllIngredients: false,
   }
 
   handleCheckboxChange(index) {
     let newChecked = this.state.checked
+    let newNumberChecked = this.state.numberChecked
+
     newChecked[index] = !this.state.checked[index]
-    this.setState({ checked: newChecked })
+    this.state.checked[index] ? newNumberChecked++ : newNumberChecked--
+    let obtained = this.state.checked.length === newNumberChecked ? true : false
+
+    this.setState({
+      checked: newChecked,
+      numberChecked: newNumberChecked,
+      obtainedAllIngredients: obtained,
+    })
+
+    if (this.state.checked.length === newNumberChecked) {
+      this.setState({ obtainedAllIngredients: true })
+    } else {
+      this.setState({ obtainedAllIngredients: false })
+    }
   }
 
   render() {
     return (
+      <Fragment>
       <ul className='container-shopping'>
         {this.props.items.direction.map((item, index) => (
           <li
@@ -33,6 +51,8 @@ class ShoppingList extends Component {
           </li>
         ))}
       </ul>
+      {this.state.obtainedAllIngredients && <button>Now, let's get cooking!</button>}
+      </Fragment>
     )
   }
 }

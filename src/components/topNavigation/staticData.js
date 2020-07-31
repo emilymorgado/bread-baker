561f96bcda8909dcc5576b5aff619e6035114ea0
Emily Morgado <emilyannemorgado@gmail.com>
2020-07-30 18:30:55 -0700

    Move static test data to a separate file

---
+++ src/components/topNavigation/staticData.js
@@ -0,0 +1,53 @@
+export const dietOptions = [
+  'vegetarian',
+  'vegan',
+  'low-fat',
+  'low-carbohydrate',
+  'paleo',
+  'ketogenic',
+  'alkaline',
+  'gluten-free',
+  'low-FODMAP',
+  'Mediterranean',
+  'MIND',
+  'DASH',
+]
+
+export const commonAllergies = [
+  'milk',
+  'eggs',
+  'tree nuts',
+  'peanuts',
+  'wheat',
+  'soy',
+  'shellfish',
+  'fish',
+  'sesame',
+]
+
+export const testDiets = [
+  true,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  true,
+  false,
+  true,
+  false
+]
+
+export const testAllergies = [
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  true
+]
